c59afbc9498a9c12543fd8dd728d5268ab72f74a
Behzad <saeedibehzad@gmail.com>
2021-09-16 21:01:28 +0430

    set state in transactionForm in brtter way

--- src/components/ExpenseApp.jsx
+++ src/components/ExpenseApp.jsx
@@ -6,8 +6,9 @@ const ExpenseApp = () => {
     const[income,setIncome]=useState(0)
     const[expense,setExpense]=useState(0)
     const[trasactions,setTransactions]=useState([])
-    const addTransactionHandler=()=>{
-        console.log("ok");
+    const addTransactionHandler=(formValue)=>{
+       const data = {...formValue , id: Date.now()}
+       setTransactions([...trasactions,data])
     }
     return ( 
         <OverWiewComponent income={income} expense={expense} onAddTransaction2={addTransactionHandler}/>
--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,30 +1,42 @@
 import { useState } from "react";
 
-const TransactionForm = ({onAddTransaction}) => {
-    const[desc,setDesc]=useState("")
-    const[amount,setAmount]=useState(0)
-    const[type,setType]=useState("")
-
-    const descHandler=(e)=>{
-        console.log(e.target.value);
-        setDesc(e.target.value)
-    }
-    const amountHandler=(e)=>{
-        console.log(e.target.value);
-        setAmount(e.target.value)
-    }
-    //console.log(type);
+const TransactionForm = ({ onAddTransaction }) => {
+  const [formValue, setFormValue] = useState({
+    type: "expense",
+    desc: "",
+    amount: 0,
+  });
+  const changeHandler = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  };
+  //console.log(type);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    onAddTransaction(formValue);
+  };
   return (
-    <form >
-      <input type="text" name="desc" onChange={(e)=>descHandler(e)}/>
-      <input type="number" name="amount" onChange={(e)=>amountHandler(e)}/>
+    <form onSubmit={submitHandler}>
+      <input type="text" name="desc" onChange={changeHandler} />
+      <input type="number" name="amount" onChange={changeHandler} />
       <div>
-        <input type="radio" value="Expense" name="type" onClick={()=>setType("expense")}/>
+        <input
+          type="radio"
+          value="expense"
+          name="type"
+          checked={formValue.type==="expense"}
+          onClick={changeHandler}
+        />
         <label>Expense</label>
-        <input type="radio" value="Income" name="type" onClick={()=>setType("income")} />
+        <input
+          type="radio"
+          value="income"
+          name="type"
+          checked={formValue.type==="income"}
+          onClick={changeHandler}
+        />
         <label>Income</label>
       </div>
-      <button type="submit" onClick={onAddTransaction}>Add Transaction</button>
+      <button type="submit">Add Transaction</button>
     </form>
   );
 };
