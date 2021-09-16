c3b82946b2f905da8aead6ae4c3405eff13daaf3
Behzad <saeedibehzad@gmail.com>
2021-09-16 17:36:34 +0430

    expense styles added

--- src/components/ExpenseApp.jsx
+++ src/components/ExpenseApp.jsx
@@ -5,12 +5,12 @@ const ExpenseApp = () => {
     const[income,setIncome]=useState(0)
     const[expense,setExpense]=useState(0)
     return ( 
-        <section>
-            <div>
-                <h2>Balance <span>{income - expense}</span></h2>
+        <section className="container">
+            <div className="topSection">
+                <h2>Balance : <span>{income - expense}</span></h2>
                 <button>Add</button>
             </div>
-            <div>
+            <div className="bottomSection">
                 <h2>Expense <span>{expense}</span></h2>
                 <h2>Income <span>{income}</span></h2>
             </div>
